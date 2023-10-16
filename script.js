c3a6486383458d072d79f969a948fde59a53431f
takke <107464146+takke-a@users.noreply.github.com>
2023-10-16 15:06:34 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const main = document.querySelector('#main-image');
+const thumbs = document.querySelectorAll('.thumb');
+
+// 確認
+console.log(main);
+console.log(thumbs);
+
+thumbs.forEach(function(item, index) {
+  item.onclick = function() {
+    console.log(this.dataset.image);
+  }
+});
+thumbs.forEach(function(item, index) {
+  item.onmouseover = function() {
+    // console.log(this.dataset.image);
+    main.src = this.dataset.image;
+    this.textContent = this.title;
+  }
+});
